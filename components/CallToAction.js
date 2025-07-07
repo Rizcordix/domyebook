bad3ddfa48bd223db425534329694b36251f3b10
Murtaza445 <murtazajohar123@gmail.com>
2025-07-07 14:40:40 +0500

    errorcta

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -51,3 +51,37 @@ export const CallToAction1 = () => {
     </div>
   );
 };
+
+export const CallToAction2 = () => {
+  return (
+    <div className="mil-cta mil-up">
+      <div className="container">
+        <div
+          className="mil-out-frame mil-p-160-160"
+          style={{ backgroundImage: "url(img/home-3/5.png)" }}
+        >
+          <div className="row justify-content-between align-items-center">
+            <div className="col-xl-7 mil-sm-text-center">
+              <h2 className="mil-light mil-mb-30 mil-up">
+                Discover the freedom <br />
+                of Total Financial Control
+              </h2>
+              <p className="mil-text-m mil-mb-60 mil-dark-soft mil-up">
+                Join Plax and take the first step towards a more <br /> balanced
+                and hassle-free financial life.
+              </p>
+              <div className="mil-buttons-frame mil-up">
+                <a href="https://apps.apple.com/" target="_blank" className="mil-btn mil-md">
+                  App Store
+                </a>
+                <a href="https://play.google.com/" target="_blank" className="mil-btn mil-border mil-md">
+                  Google Play
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
